f1ef891cc877799b7644b8114f35e5aab007e2ab
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-08-22 22:02:07 -0700

    Implementing Calculations Completed

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Yoko Yamaguchi on 8/22/15.
+//  Copyright (c) 2015 Yoko Yamaguchi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
