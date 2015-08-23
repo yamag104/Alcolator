a596ef8220f78bb4d504bcddc5336e5464133931
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-08-22 21:28:37 -0700

    Checkpoint 18 Storyboarding Completed

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Yoko Yamaguchi on 8/22/15.
+//  Copyright (c) 2015 Yoko Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
