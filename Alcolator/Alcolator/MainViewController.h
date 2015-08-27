9721a85c74e854bd5bb54957d06c69c41273a703
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-08-26 21:43:23 -0700

    Trying to merge

---
+++ Alcolator/Alcolator/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Alcolator
+//
+//  Created by Yoko Yamaguchi on 8/25/15.
+//  Copyright (c) 2015 Yoko Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Alcolator/Alcolator/MainViewController.m
@@ -0,0 +1,13 @@
+//
+//  MainViewController.m
+//  Alcolator
+//
+//  Created by Yoko Yamaguchi on 8/25/15.
+//  Copyright (c) 2015 Yoko Yamaguchi. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+@end
